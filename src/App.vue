10dbe905a31751bf525e96cdfd22a0c92bae309f
Calliope Joselyn <calliopecobos@gmail.com>
2025-06-10 11:48:50 -0600

    Powerbi

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,9 @@
   <p>Visualización interactiva de
     datos generada en Power BI.</p>
   <div class="iframe-container">
-
-<h1>HOLA CALI</h1>
-       
+    <iframe title="VisualizaciónDataMartNorthwind" width="600" height="373.5" 
+    src="https://app.powerbi.com/view?r=eyJrIjoiZjE3Y2IwYzQtZjEwYy00MWZlLWE1ODMtN2Q0ZDA3YjQ5ZDkyIiwidCI6IjMzN2U0N2Y4LWQ1MDUtNDZkMC05ZGNjLWRmMzY5ODg2Y2RiZiJ9" 
+    frameborder="0" allowFullScreen="true"></iframe>
   </div>
 </div>
 </template>
